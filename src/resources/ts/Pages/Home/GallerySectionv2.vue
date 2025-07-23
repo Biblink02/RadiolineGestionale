7da504b4ae704f51bcbcb3b10ebd5fcad2fb9356
Biblink <alby.bot.24@gmail.com>
2025-07-23 09:50:33 +0200

    feat: gallery proposals

---
+++ src/resources/ts/Pages/Home/GallerySectionv2.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const images = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAlaRIJqVwMC2ym91g3pO53kyPByMZR9j_7Q&s',
+    'https://www.operaromanapellegrinaggi.org/uploads/slideshow_item/mobile_image/1402/MDJGR-0-TESTATA-MOBILE-435x465.jpg',
+    'https://www.madonnadellabozzola.org/wp-content/uploads/2024/04/Medjugorje-24-scaled.jpg',
+    'https://www.sanfrancescodelprato.it/wp-content/uploads/2025/03/Medju2024-1024x578.jpg',
+    'https://rusconiviaggi.it/wp-content/uploads/2024/07/Pellegrinaggio-a-Medjugorje-da-Verona_Rusconi-Viaggi-e-Pellegrinaggi.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAlaRIJqVwMC2ym91g3pO53kyPByMZR9j_7Q&s',
+];
+</script>
+
+<template>
+    <section class="py-12 bg-gray-100">
+        <div class="max-w-5xl mx-auto px-4">
+            <h2 class="text-3xl font-bold text-center mb-6">Galleria di Medjugorje</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <Image
+                    v-for="(img, index) in images"
+                    :key="index"
+                    :src="img"
+                    alt="Medjugorje"
+                    preview
+                    imageClass="w-full h-60 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform"
+                >
+                    <template #indicatoricon>
+                        <i class="pi pi-search"></i>
+                    </template>
+                </Image>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+</style>
--- src/resources/ts/Pages/Home/HomePage.vue
+++ src/resources/ts/Pages/Home/HomePage.vue
@@ -3,12 +3,26 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import HeroSection from "@/Pages/Home/HeroSection.vue";
 import AboutSection from "@/Pages/Home/AboutSection.vue";
+import GallerySectionv1 from "@/Pages/Home/GallerySectionv1.vue";
+import GallerySectionv2 from "@/Pages/Home/GallerySectionv2.vue";
+import GallerySectionv3 from "@/Pages/Home/GallerySectionv3.vue";
 </script>
 
 <template>
-    <AppLayout :title="'Homepage'">
+    <AppLayout title="Homepage">
         <HeroSection/>
         <AboutSection/>
+        <p class="text-center">PROPOSTA GALLERIA IMMAGINI:</p>
+        <p class="text-center">v1:</p>
+        <GallerySectionv1/>
+        <br>
+        <p class="text-center">v2:</p>
+
+        <GallerySectionv2/>
+        <br>
+        <p class="text-center">v3:</p>
+
+        <GallerySectionv3/>
     </AppLayout>
 </template>
 
