d759e1f74d37acc21f55528cc821d191ee35e2d8
Biblink <alby.bot.24@gmail.com>
2025-08-02 23:23:07 +0200

    feat: favicons

--- src/resources/ts/Layouts/AppLayout.vue
+++ src/resources/ts/Layouts/AppLayout.vue
@@ -16,6 +16,13 @@ const props = defineProps<{
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+            <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+            <link rel="shortcut icon" href="/favicons/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="MdjService" />
+            <link rel="manifest" href="/favicons/site.webmanifest" />
+            <meta name="theme-color" content="#ffffff">
         </Head>
         <div class="min-h-screen">
 
