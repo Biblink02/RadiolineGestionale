fab53cf2ec75247b87d23421cbacbb787cb5f5be
Biblink <alby.bot.24@gmail.com>
2025-08-13 13:03:44 +0200

    feat: finish

--- src/resources/ts/Pages/Home/HeroSection.vue
+++ src/resources/ts/Pages/Home/HeroSection.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
 const {t} = useI18n()
-import Home1 from "@/../media/1-home-temp.jpg";
-import Home_mobile from "@/../media/1-home-mobile.jpg";
+import Home_large from "@/../media/1-home-large.png";
+import Home_mobile from "@/../media/1-home-mobile.png";
 
 </script>
 
 <template>
-    <div class="relative text-white sm:block hidden">
-        <img :src="Home1" class="mb-20 aspect-auto max-w-3xl mx-auto w-full"/>
-        <div class="container absolute top-0 left-30 mx-auto px-6">
-            <h1 class="text-3xl font-bold md:text-4xl max-w-2xl mt-8 mx-auto">
+    <div class="relative text-white sm:block hidden max-w-3xl mx-auto">
+        <img :alt="t('home.hero.title_sr')" :src="Home_large" class="mb-20 aspect-auto w-full"/>
+
+        <div class="absolute left-8/25 top-1/5 transform -translate-x-1/2 -translate-y-1/2 w-fit text-center">
+            <h1 class="text-3xl font-bold md:text-4xl max-w-2xl">
                 {{ t('home.hero.title') }}:
                 <span class="sr-only">{{ t('home.hero.title_sr') }}</span>
             </h1>
 
-            <p class="max-w-2xl mx-auto text-lg mt-2 text-xl">
+            <p class="max-w-2xl text-lg mt-2 text-xl">
                 {{ t('home.hero.subtitle1') }}, <br> {{ t('home.hero.subtitle2') }}.
             </p>
         </div>
+
     </div>
+
     <div class="relative text-white sm:hidden">
-        <img :src="Home_mobile" class="mb-20 aspect-auto max-w-3xl mx-auto h-[60vh] w-full"/>
-        <div class="container absolute top-22 left-15 mx-auto px-6">
+        <img :src="Home_mobile" :alt="t('home.hero.title_sr')" class="mb-6 h-[60vh] w-full"/>
+        <div class="w-fit absolute top-26 left-15 mx-auto px-6">
             <h1 class="text-xl font-bold md:text-4xl max-w-2xl mt-8 mx-auto">
                 {{ t('home.hero.title') }}:
                 <span class="sr-only">{{ t('home.hero.title_sr') }}</span>
